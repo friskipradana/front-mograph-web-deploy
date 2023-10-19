1b2426f18e4361cde5218104a45f3c4dce01aa7e
friskipradana <friskipradana8@gmail.com>
2023-10-19 12:19:30 +0700

    Updates

---
+++ precache-manifest.741a1869418377cb4a30155c173ff1bd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e76483d178d49145f53ffd1d0235a2a5",
+    "url": "/front-mograph-web-deploy/index.html"
+  },
+  {
+    "revision": "3fef518c28f369dd3a60",
+    "url": "/front-mograph-web-deploy/static/css/2.7593e019.chunk.css"
+  },
+  {
+    "revision": "9d89cebf427fe0cd2df2",
+    "url": "/front-mograph-web-deploy/static/css/main.50738228.chunk.css"
+  },
+  {
+    "revision": "3fef518c28f369dd3a60",
+    "url": "/front-mograph-web-deploy/static/js/2.13e3b560.chunk.js"
+  },
+  {
+    "revision": "ed62956a15f956c5d13500365caee991",
+    "url": "/front-mograph-web-deploy/static/js/2.13e3b560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea1bb54c388c1e6fb19",
+    "url": "/front-mograph-web-deploy/static/js/3.13922520.chunk.js"
+  },
+  {
+    "revision": "9d89cebf427fe0cd2df2",
+    "url": "/front-mograph-web-deploy/static/js/main.e28f6a14.chunk.js"
+  },
+  {
+    "revision": "22e97a03a6ce45e41c1a",
+    "url": "/front-mograph-web-deploy/static/js/runtime-main.b600c435.js"
+  },
+  {
+    "revision": "093ee89be9ede30383f39a899c485a82",
+    "url": "/front-mograph-web-deploy/static/media/Poppins-Regular.093ee89b.ttf"
+  },
+  {
+    "revision": "324d28065163e887bab5d9b8ed6670a3",
+    "url": "/front-mograph-web-deploy/static/media/ellipse_object.324d2806.svg"
+  },
+  {
+    "revision": "b9d2b8827f4704f58b9a35c96bb645a4",
+    "url": "/front-mograph-web-deploy/static/media/email.b9d2b882.svg"
+  },
+  {
+    "revision": "fd23f35060b8b93215da0244802b637f",
+    "url": "/front-mograph-web-deploy/static/media/location-no-color.fd23f350.svg"
+  },
+  {
+    "revision": "f12e6fe19846f5ee8e39304aadeeb90a",
+    "url": "/front-mograph-web-deploy/static/media/my_photo.f12e6fe1.png"
+  },
+  {
+    "revision": "25cc5745d9f2c42107ad35c3dd915d1a",
+    "url": "/front-mograph-web-deploy/static/media/phone.25cc5745.svg"
+  },
+  {
+    "revision": "cae5c130902486973617bb001f907444",
+    "url": "/front-mograph-web-deploy/static/media/polygon_object.cae5c130.svg"
+  },
+  {
+    "revision": "9622005941e966fbfab8df12149a36e4",
+    "url": "/front-mograph-web-deploy/static/media/rectangle.96220059.svg"
+  },
+  {
+    "revision": "5725e35d73b414f18145a0eb7c29eda1",
+    "url": "/front-mograph-web-deploy/static/media/rectangle_object.5725e35d.svg"
+  },
+  {
+    "revision": "3981bb1ce95aaf1c2829bb69a9c3f2a1",
+    "url": "/front-mograph-web-deploy/static/media/search.3981bb1c.svg"
+  },
+  {
+    "revision": "5c367804d8a53b3486bec2bfed059643",
+    "url": "/front-mograph-web-deploy/static/media/search_white.5c367804.svg"
+  }
+]);
